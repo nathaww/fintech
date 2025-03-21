8bde44d3b42f759e13c099d58da474a3b8a3b516
Nathan Solomon Zegeye <solomonnathan065@gmail.com>
2025-03-21 20:16:55 +0300

    - Created 5 tabs - Removed expo-dev-tools package to fix Expo app not opening problem

--- app/(tabs)/invest.tsx
+++ app/(tabs)/invest.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet } from 'react-native';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
